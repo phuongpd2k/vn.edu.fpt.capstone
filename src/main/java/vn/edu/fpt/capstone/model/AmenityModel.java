c45020a7a78e74f0dd50ada3d53e023079d9a833
Phạm Duy Phương <zuyfun@gmail.com>
2022-05-14 22:05:45 +0700

    Disable CORS

--- src/main/java/vn/edu/fpt/capstone/model/AmenityModel.java
+++ src/main/java/vn/edu/fpt/capstone/model/AmenityModel.java
@@ -2,10 +2,7 @@ package vn.edu.fpt.capstone.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
