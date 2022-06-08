61a54350e7c17a1d9661683f8390f515481e47bf
Phạm Duy Phương <zuyfun@gmail.com>
2022-06-08 13:16:03 +0700

    UPDATE DISABLE WORTH TABLE

--- src/main/java/vn/edu/fpt/capstone/model/RoomAmenitiesModel.java
+++ src/main/java/vn/edu/fpt/capstone/model/RoomAmenitiesModel.java
@@ -1,23 +1,23 @@
-package vn.edu.fpt.capstone.model;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-import javax.persistence.*;
-
-@Data
-@Entity
-@Table(name = "ROOM_AMENITIES")
-@EqualsAndHashCode(callSuper = false)
-public class RoomAmenitiesModel extends Auditable<String>{
-    @Id
-    @Column(name = "ID")
-    @GeneratedValue(generator = "ROOM_AMENITIES_SeqGen", strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "ROOM_AMENITIES_SeqGen", sequenceName = "ROOM_AMENITIES_Seq",allocationSize=1)
-    private Long id;
-    @Column(name = "ROOMID")
-    private Long houseId;
-    @Column(name = "AMENITYID")
-    private Long amenityId;
-
-}
+//package vn.edu.fpt.capstone.model;
+//
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//
+//import javax.persistence.*;
+//
+//@Data
+//@Entity
+//@Table(name = "ROOM_AMENITIES")
+//@EqualsAndHashCode(callSuper = false)
+//public class RoomAmenitiesModel extends Auditable<String>{
+//    @Id
+//    @Column(name = "ID")
+//    @GeneratedValue(generator = "ROOM_AMENITIES_SeqGen", strategy = GenerationType.SEQUENCE)
+//    @SequenceGenerator(name = "ROOM_AMENITIES_SeqGen", sequenceName = "ROOM_AMENITIES_Seq",allocationSize=1)
+//    private Long id;
+//    @Column(name = "ROOMID")
+//    private Long houseId;
+//    @Column(name = "AMENITYID")
+//    private Long amenityId;
+//
+//}
--- src/main/java/vn/edu/fpt/capstone/repository/RoomCategoryRepository.java
+++ src/main/java/vn/edu/fpt/capstone/repository/RoomCategoryRepository.java
@@ -2,7 +2,6 @@ package vn.edu.fpt.capstone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import vn.edu.fpt.capstone.model.RoomAmenitiesModel;
 import vn.edu.fpt.capstone.model.RoomCategoryModel;
 @Repository
 public interface RoomCategoryRepository extends JpaRepository<RoomCategoryModel,Long> {
