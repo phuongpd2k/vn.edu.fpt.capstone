db468a579314cb99a44035c3d1e7a00b8487aa55
dungtv <truongdung07062000@gmail.com>
2022-06-09 00:15:06 +0700

    [DungTVHE140366]:crud api  post type

--- src/main/java/vn/edu/fpt/capstone/controller/PostTypeController.java
+++ src/main/java/vn/edu/fpt/capstone/controller/PostTypeController.java
@@ -17,15 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.edu.fpt.capstone.constant.Message;
-import vn.edu.fpt.capstone.dto.AmenityDto;
 import vn.edu.fpt.capstone.dto.ListIdDto;
 import vn.edu.fpt.capstone.dto.PostTypeDto;
 import vn.edu.fpt.capstone.dto.ResponseObject;
+import vn.edu.fpt.capstone.model.PostTypeModel;
 import vn.edu.fpt.capstone.service.PostTypeService;
 
 @RestController
 @RequestMapping("/api/post-type")
 @CrossOrigin(origins = "*", maxAge = 3600)
+//DungTV
 public class PostTypeController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PostTypeController.class.getName());
 	
@@ -51,70 +52,85 @@ public class PostTypeController {
 	}
 	
 	@PostMapping()
-	public ResponseEntity<?> postAmenity(@RequestBody AmenityDto amenityDto) {
-		ResponseObject response = new ResponseObject();
+	public ResponseEntity<?> postPostType(@RequestBody PostTypeDto postTypeDto) {
 		try {
-//			if (amenityDto.getId() != null) {
-//				LOGGER.error("postAmenity: {}","Wrong body format");
-//				response.setCode("406");
-//				response.setMessageCode(Message.NOT_ACCEPTABLE);
-//				return new ResponseEntity<>(response, HttpStatus.NOT_ACCEPTABLE);
-//			}
-//			AmenityDto amenityDto2 = amenityService.createAmenity(amenityDto);
-//			if (amenityDto2 == null) {
-//				response.setCode("500");
-//				response.setMessageCode(Message.INTERNAL_SERVER_ERROR);
-//				return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-//			}
-//			response.setCode("200");
-//			response.setMessageCode(Message.OK);
-//			response.setResults(amenityDto2);
-//			LOGGER.info("postAmenity: {}",amenityDto2);
-			return new ResponseEntity<>(response, HttpStatus.OK);
+			if(postTypeDto.getPrice() < 0) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(ResponseObject.builder().code("400").message("Create post type: price < 0!")
+								.messageCode("CREATE_POST_TYPE_FAIL").build());
+			}
+			
+			if(postTypeDto.getType().trim().isEmpty()) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(ResponseObject.builder().code("400").message("Create post type: type null")
+								.messageCode("CREATE_POST_TYPE_FAIL").build());
+			}
+			
+			PostTypeModel postTypeModel = postTypeService.create(postTypeDto);
+
+			if (postTypeModel != null) {
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(ResponseObject.builder().code("200").message("Create post type: successfully!")
+								.messageCode("CREATE_POST_TYPE_SUCCESSFULLY").build());
+			}
+			throw new Exception();
 		} catch (Exception e) {
-			LOGGER.error("postAmenity: {}",e);
-			response.setCode("500");
-			response.setMessageCode(Message.INTERNAL_SERVER_ERROR);
-			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			LOGGER.error("postPostType: {}",e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(ResponseObject.builder().code("500").message("Create post type: fail!")
+							.messageCode("CREATE_POST_TYPE_FAIL").build());
 		}
 	}
 	
 	@PutMapping()
-	public ResponseEntity<ResponseObject> putAmenity(@RequestBody AmenityDto amenityDto) {
-		ResponseObject response = new ResponseObject();
+	public ResponseEntity<?> putPostType(@RequestBody PostTypeDto postTypeDto) {
 		try {
-//			if (amenityDto.getId() == null || !amenityService.isExist(amenityDto.getId())) {
-//				LOGGER.error("putAmenity: {}","ID Amenity is not exist");
-//				response.setCode("404");
-//				response.setMessageCode(Message.NOT_FOUND);
-//				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-//			}
-//			AmenityDto amenityDto2 = amenityService.updateAmenity(amenityDto);
-//			response.setCode("200");
-//			response.setMessageCode(Message.OK);
-//			response.setResults(amenityDto2);
-//			LOGGER.info("putAmenity: {}",amenityDto2);
-			return new ResponseEntity<>(response, HttpStatus.OK);
+			if(postTypeDto.getId() == null) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(ResponseObject.builder().code("400").message("Update post type: id null")
+								.messageCode("UPDATE_POST_TYPE_FAIL").build());
+			}
+			
+			if(postTypeDto.getPrice() < 0) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(ResponseObject.builder().code("400").message("Update post type: price < 0!")
+								.messageCode("UPDATE_POST_TYPE_FAIL").build());
+			}
+			
+			if(postTypeDto.getType().trim().isEmpty()) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(ResponseObject.builder().code("400").message("Update post type: type null")
+								.messageCode("UPDATE_POST_TYPE_FAIL").build());
+			}
+			
+			PostTypeModel postTypeModel = postTypeService.create(postTypeDto);
+
+			if (postTypeModel != null) {
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(ResponseObject.builder().code("200").message("Update post type: successfully!")
+								.messageCode("UPDATE_POST_TYPE_SUCCESSFULLY").build());
+			}
+			throw new Exception();
 		} catch (Exception e) {
-			LOGGER.error("putAmenity: {}",e);
-			response.setCode("500");
-			response.setMessageCode(Message.INTERNAL_SERVER_ERROR);
-			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			LOGGER.error("putPostType: {}",e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(ResponseObject.builder().code("500").message("Update post type: fail!")
+							.messageCode("UPDATE_POST_TYPE_FAIL").build());
 		}
 	}
 	
 	@DeleteMapping()
-	public ResponseEntity<?> deleteListAmenity(@RequestBody ListIdDto listIdDto) {
+	public ResponseEntity<?> deletePostType(@RequestBody ListIdDto listIdDto) {
 		try {
-			//amenityService.removeListAmenity(listIdDto);
+			postTypeService.removePostType(listIdDto);
 			return ResponseEntity.status(HttpStatus.OK)
-					.body(ResponseObject.builder().code("200").message("Delete list amenity successfully!")
-							.messageCode("DELETE_LIST_AMENITY_SUCCESSFULL").build());
+					.body(ResponseObject.builder().code("200").message("Delete list post type successfully!")
+							.messageCode("DELETE_POST_TYPE_SUCCESSFULL").build());
 		} catch (Exception e) {
 			LOGGER.error(e.toString());
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-					.body(ResponseObject.builder().code("400").message("Delete list amenity fail!")
-							.messageCode("DELETE_LIST_AMENITY_FAIL").build());
+					.body(ResponseObject.builder().code("400").message("Delete list post type fail!")
+							.messageCode("DELETE_POST_TYPE_FAIL").build());
 		}
 	}
 }
--- src/main/java/vn/edu/fpt/capstone/service/impl/PostTypeServiceImpl.java
+++ src/main/java/vn/edu/fpt/capstone/service/impl/PostTypeServiceImpl.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import vn.edu.fpt.capstone.dto.ListIdDto;
 import vn.edu.fpt.capstone.dto.PostTypeDto;
 import vn.edu.fpt.capstone.model.PostTypeModel;
 import vn.edu.fpt.capstone.repository.PostTypeRepository;
@@ -20,6 +23,8 @@ public class PostTypeServiceImpl implements PostTypeService{
 	
 	@Autowired
 	private ModelMapper modelMapper;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(PostTypeServiceImpl.class.getName());
 	
 	@Override
 	public PostTypeModel getById(Long id) {
@@ -37,6 +42,27 @@ public class PostTypeServiceImpl implements PostTypeService{
 		
 		List<PostTypeDto> listDto = Arrays.asList(modelMapper.map(list, PostTypeDto[].class));
 		return listDto;
+	}
+
+	@Override
+	public PostTypeModel create(PostTypeDto postTypeDto) {
+		PostTypeModel postTypeModel = modelMapper.map(postTypeDto, PostTypeModel.class);
+		return postTypeRepository.save(postTypeModel);
+	}
+
+	@Override
+	public void removePostType(ListIdDto listIdDto) {
+		try {
+			for (Long id : listIdDto.getList()) {
+				PostTypeModel postTypeModel = postTypeRepository.getById(id);
+				if(postTypeModel != null) {
+					postTypeModel.setEnable(false);
+					postTypeModel = postTypeRepository.save(postTypeModel);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error("deletePostType: {}", e.getMessage());
+		}	
 	}
 
 }
