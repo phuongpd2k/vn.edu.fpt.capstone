bafb204be478b94fecec974e23e4bc6535ecabc4
Phạm Duy Phương <zuyfun@gmail.com>
2022-06-23 00:30:00 +0700

    UPDATE Refer Database

--- src/main/java/vn/edu/fpt/capstone/repository/PhuongXaRepository.java
+++ src/main/java/vn/edu/fpt/capstone/repository/PhuongXaRepository.java
@@ -3,11 +3,14 @@ package vn.edu.fpt.capstone.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import vn.edu.fpt.capstone.model.PhuongXaModel;
 
 @Repository
 public interface PhuongXaRepository extends JpaRepository<PhuongXaModel, Long>{
-	List<PhuongXaModel> findAllByMaQh(Long maQh);
+	@Query(value = "select * from phuong_xa where maqh= :maqh", nativeQuery = true)
+	List<PhuongXaModel> findAllByMaQh(@Param("maqh") Long maQh);
 }
