90aa1c664574cd48d4fe0828357ff6757b984591
dungtv <truongdung07062000@gmail.com>
2022-06-30 12:50:31 +0700

    [DungTVHE140366]:update response api transaction

--- src/main/java/vn/edu/fpt/capstone/response/TransactionResponse.java
+++ src/main/java/vn/edu/fpt/capstone/response/TransactionResponse.java
@@ -11,6 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionResponse {
+	private Long id;
+	private Long userId;
 	private String fullname;
 	private String username;
 	private float amount;
